26c58fbf0b77ffaf07b84567924fbd8a509160ab
paras-verma <paras.19508@sscbs.du.ac.in>
2023-05-30 22:38:33 +0530

    fix: tsup.clean doesn't remove folder @ outDir
    note: use npm scripts instead

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tsup";
 const isDev = process.env.npm_lifecycle_event === "dev";
 
 export default defineConfig({
-  clean: true,
   entry: ["cli/index.ts"],
   format: ["esm"],
   minify: !isDev,
